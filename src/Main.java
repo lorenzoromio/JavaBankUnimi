2b90e333ad6f71f076ff5e568f7b2283b7ec4597
lorenzoromio <32493122+lorenzoromio@users.noreply.github.com>
2020-08-27 21:56:58 +0200

    addAccount for populate DB for testing

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,16 @@
  * Copyright (c) 2020 Lorenzo Romio. All Right Reserved.
  */
 
+import javax.security.auth.login.AccountException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)  {
+
+//            DBConnect.deleteAll();
+        DBConnect.populateDB();
 
 
 //        new WebApp("luca.armaroli","kiara4Lif3!");
