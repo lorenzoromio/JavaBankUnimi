21eede72aa79cfefdf19e94cebe95703d633022e
lorenzoromio <32493122+lorenzoromio@users.noreply.github.com>
2020-08-30 21:37:39 +0200

    cleanup

--- src/Bank.java
+++ src/Bank.java
@@ -5,7 +5,6 @@
 import javax.security.auth.login.AccountException;
 import javax.security.auth.login.AccountNotFoundException;
 import javax.security.auth.login.CredentialException;
-import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,24 +15,17 @@ public class Bank {
     private static final String cab = "03";
 
 
-
     public static void addAccount(Account account) throws IllegalArgumentException, SQLException, AccountException {
-
-//        DBConnect db = new DBConnect();
         String checkUser = "select ID from accounts where username = ?";
         PreparedStatement prepStmt = DBConnect.getConnection().prepareStatement(checkUser);
         prepStmt.setString(1, account.getUsername());
         ResultSet rs = prepStmt.executeQuery();
         if (rs.next()) {
-            System.out.println("Account already in database");
             throw new AccountException("Account already in database");
         }
 
-        if (rs != null) rs.close();
-        prepStmt.close();
-
         String newAccount = "insert into accounts(USERNAME, NOME, COGNOME, HASHPSW, SALT, TIMESTAMP, IBAN, NUM_CONTO, SALDO) " +
-                "values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                            "values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         prepStmt = DBConnect.getConnection().prepareStatement(newAccount);
         prepStmt.setString(1, account.getUsername());
         prepStmt.setString(2, account.getNome());
@@ -43,14 +35,13 @@ public class Bank {
         prepStmt.setString(6, account.getTimestamp());
         prepStmt.setString(7, account.getIban());
         prepStmt.setString(8, account.getNum_conto());
+
         try {
             prepStmt.setDouble(9, account.getSaldo());
         } catch (TimeoutException e) {
             //
         }
         prepStmt.execute();
-        prepStmt.close();
-
     }
 
     public static Session login(String username, String login_psw) throws CredentialException, AccountNotFoundException, SQLException {
